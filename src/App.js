2d7d7865ba45caa875756173bd4b1f121612ddc2
trieunguyentm <trieunguyen241102@gmail.com>
2023-11-09 16:38:33 +0700

    add plugins

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import React from 'react';
 import './App.css';
 
+const setListPlugin = () => {
+  let listPlugin = [];
+  let plugins = navigator.plugins;
+  if (plugins.length > 0) {
+    for (let i = 0; i < plugins.length; i++) {
+      listPlugin.push(plugins[i]);
+    }
+  }
+  return listPlugin;
+};
+
+
 function App() {
-  const screenHeight = window.screen.height
-  const screenWidth = window.screen.width
+  const screenHeight = window.screen.height;
+  const screenWidth = window.screen.width;
+  const listPlugin = setListPlugin();
 
   return (
     <div className="App">
+      <div>{navigator.platform}</div>
+      <div>{`${screenHeight}x${screenWidth}`}</div>
       <div>
-        {navigator.platform}
-      </div>
-      <div>
-        {navigator.userAgent}
-      </div>
-      <div>
-        {`${screenHeight}x${screenWidth}`}
+        {listPlugin.map((plugin, index) => (
+          <div key={index}>{`${plugin.name} - ${plugin.filename}`}</div>
+        ))}
       </div>
     </div>
   );
