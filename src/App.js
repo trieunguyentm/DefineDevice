b9634c21b20946a4ce2eb109e8fc503dec51d313
trieunguyentm <trieunguyen241102@gmail.com>
2023-11-09 17:57:04 +0700

    add style body

--- src/App.js
+++ src/App.js
@@ -13,20 +13,30 @@ const setListPlugin = () => {
 };
 
 
+// const getDefaultBackgroundColor = () => {
+//   const bodyComputedStyle = window.getComputedStyle(document.body)
+//   console.log(bodyComputedStyle.fontSize)
+//   console.log(bodyComputedStyle.backgroundColor)
+// }
+
+
 function App() {
   const screenHeight = window.screen.height;
   const screenWidth = window.screen.width;
   const listPlugin = setListPlugin();
-
+  const bodyComputedStyle = window.getComputedStyle(document.body)
   return (
     <div className="App">
       <div>{navigator.platform}</div>
       <div>{`${screenHeight}x${screenWidth}`}</div>
       <div>
         {listPlugin.map((plugin, index) => (
-          <div key={index}>{`${plugin.name} - ${plugin.filename}`}</div>
+          // <div key={index}>{`${plugin.name} - ${plugin.filename}`}</div>
+          <div key={index}>{`${plugin.name}`}</div>
         ))}
       </div>
+      <div>{bodyComputedStyle.fontSize}</div>
+      <div>{bodyComputedStyle.backgroundColor}</div>
     </div>
   );
 }
